444c6fbcf337c6a909552560f7517f0d558b2f53
akshayjadhhav18 <105843403+akshayjadhhav18@users.noreply.github.com>
2025-03-13 11:35:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 function firstWord(s) {
-   
+    // Trim leading spaces to ensure correct first word detection
+    s = s.trimStart();
+
     if (s.length === 0) {
         return s;
     }
 
-    
     const spaceIndex = s.indexOf(' ');
 
-    
     if (spaceIndex === -1) {
         return s;
     }
 
-   
     return s.substring(0, spaceIndex);
 }
 
